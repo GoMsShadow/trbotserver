6089a8482c59bf6abb58a24f01d17411f50a1704
Shadow <thor9599modric@gmail.com>
2024-11-20 16:46:40 +0900

    Fix root route

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ app.get("/api/symbol-table", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
